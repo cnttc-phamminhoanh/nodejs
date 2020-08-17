e372f214f679cde589e14363f3fa843db74afa55
cnttc-phamminhoanh <46447153+cnttc-phamminhoanh@users.noreply.github.com>
2020-08-17 17:31:45 +0700

    commit from lession 12

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var userRoute = require('./routes/user.route.js');
+
+var app = express();
+
+app.set('view engine', 'pug');
+
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
+
+var port = 3000;
+
+app.get('/',function(req,res){
+	res.render('index.pug',{
+		name : 'Mati 17CNTTC'
+	});
+});
+
+app.use('/users',userRoute);
+
+app.listen(port,function(){
+	console.log('Server listening on port ' + port);
+	console.log('Link : http://localhost:3000/');
+});
