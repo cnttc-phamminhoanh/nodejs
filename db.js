fe4e7984489e3aa80c917382c6dc469e9dbfff9c
cnttc-phamminhoanh <46447153+cnttc-phamminhoanh@users.noreply.github.com>
2020-08-27 22:19:34 +0700

    commit from lession 23 CSRF attack

--- db.js
+++ db.js
@@ -6,6 +6,10 @@ var adapter = new FileSync('db.json')
 
 var db = low(adapter)
 
-db.defaults({users: []}).write();
+db.defaults({
+	users: [],
+	sessions: [],
+	transfers: [] 
+}).write();
 
 module.exports = db;
--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var express = require('express');
 
 var cookieParser = require('cookie-parser')
 
+var csurf = require('csurf')
+
 var userRoute = require('./routes/user.route.js');
 
 var authRouter = require('./routes/auth.route.js');
 
 var cartRoute = require('./routes/cart.route.js');
+
+var transferRoute = require('./routes/transfer.route.js')
 
 var authMiddleware = require('./middlewares/auth.middleware.js');
 
@@ -30,6 +34,8 @@ app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.use(sessionMiddleware);
 
+app.use(csurf({cookie:true}));
+
 app.use(express.static('public'));
 
 var port = 3000;
@@ -47,6 +53,8 @@ app.use('/users',authMiddleware.requireAuth,userRoute);
 app.use('/products',productRoute);
 
 app.use('/cart',cartRoute);
+
+app.use('/transfer',authMiddleware.requireAuth,transferRoute);
 
 app.listen(port,function(){
 	console.log('Server listening on port ' + port);
